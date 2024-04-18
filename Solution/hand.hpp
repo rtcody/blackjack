48788da8086cb1e8d572c60b507e1b4cd45e401b
Ho-Quan <166866483+Ho-Quan@users.noreply.github.com>
2024-04-18 13:54:35 -0700

    Update hand.hpp

--- Solution/hand.hpp
+++ Solution/hand.hpp
@@ -1,37 +1,44 @@
 #pragma once
 
+#include <SFML/Graphics.hpp>
 #include "deck.hpp" 
 
 //Joey working on hand 
-class Hand 
+class Hand
 {
 
-public: 
+public:
 	/// <summary>
 	/// Hand constructor that takes in game deck 
 	/// </summary>
 	/// <param name="gameDeck"></param>
-	Hand(Deck &gameDeck); 
+	Hand(Deck& gameDeck);
 	//virtual void displayHand() = 0; // will have a different display hand in both the player and dealer class
 	/// <summary>
 	/// computes the numerical value of the entire hand
 	/// </summary>
 	/// <returns>the total value of the hand</returns>
-	int computeHandValue(); 
+	void computeHandValue();
+	int computeHandValue(card dummyHand[11]);
 	/// <summary>
 	/// adds a card to the blackjackHand
+	/// 
 	/// </summary>
 	/// <param name="gameDeck"></param>
-	void hit(Deck &gameDeck);
+	void hit(Deck& gameDeck);
+	
 	/// <summary>
 	/// virtual display hand (different for both player and dealer 
 	/// </summary>
-	//virtual void displayHand() = 0;  
-	
+	virtual void displayHand(sf::RenderWindow& window) = 0;
 
-private: 
+	card getCard(int index);
+	int getHandValue();
+
+
+protected: //changed to protected by Ben
 	card blackjackHand[11]; // 11 is the most cards you can have (All Aces) 
 	int cardCount; // used to ensure we do not go out of bounds in the array; 
 	int handValue; // total value of the hand 
-	
+
 };
